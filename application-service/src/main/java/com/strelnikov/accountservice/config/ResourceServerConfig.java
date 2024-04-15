d828294f08a3d204f368076eb30bc060b98f169a
strelnikov <evstrelnikov@gmail.com>
2024-04-14 22:12:51 -0400

    update security configuration

--- application-service/src/main/java/com/strelnikov/accountservice/config/ResourceServerConfig.java
+++ application-service/src/main/java/com/strelnikov/accountservice/config/ResourceServerConfig.java
@@ -1,25 +1,21 @@
 package com.strelnikov.accountservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ResourceServerConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .securityMatcher("/users/**")
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest()
-                        .hasAuthority("SCOPE_server"))
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .securityMatcher("/users/**")
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest()
+//                        .hasAuthority("SCOPE_server"))
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+//
+//        return http.build();
+//    }
 }
